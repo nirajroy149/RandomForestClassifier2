8711efd9accc7d54db539220b48420bbb5c86358
Niraj Kumar Roy <nirajroy149@gmail.com>
2024-06-01 23:13:44 +0530

    second commit

--- RandomForestClassifier.py
+++ RandomForestClassifier.py
@@ -55,16 +55,40 @@ with open('scaler.pkl', 'rb') as f:
 with open('model.pkl', 'rb') as f:
     loaded_model = pickle.load(f)
 
-# Create a file uploader for user input
-st.write("## Upload Audio File")
+# Create a form for user input
+st.write("## Predict Bird Type")
 
-uploaded_file = st.file_uploader("Upload an audio file", type=["wav"])
+# Feature names and default values
+feature_names = [
+    "zcr_mean", "zcr_std", "spectral_centroid", "mean_spectral_rolloff", "std_spectral_rolloff",
+    "spectral_bandwidth", "chroma1", "chroma2", "chroma3", "chroma4", "chroma5", "chroma6", 
+    "chroma7", "chroma8", "chroma9", "chroma10", "chroma11", "chroma12", "mfcc1", "mfcc2", "mfcc3", "mfcc4", "mfcc5", "mfcc6", "mfcc7", 
+    "mfcc8", "mfcc9", "mfcc10", "mfcc11", "mfcc12", "mfcc13", "mfcc14", "mfcc15", "mfcc16", 
+    "mfcc17", "mfcc18", "mfcc19", "mfcc20", "mfcc21", "mfcc22", "mfcc23", "mfcc24", "mfcc25", "mfcc26"
+]
 
-# Make prediction based on uploaded audio file
-if uploaded_file is not None:
-    audio_data, _ = librosa.load(uploaded_file, sr=None)
-    features = extract_features(audio_data)  # Define this function to extract audio features
-    user_data = loaded_scaler.transform(np.array([features]))
+default_values = [
+    0.351091,	0.051202,	4376.084267,	6666.248380,	1496.420809,	2154.197311,	0.280201,	0.404125,	0.513666,-3.597658,	-6.942390,	2.342945,	-4.922446,	10.159241,	-1.919533,	-1.433039,	-3.299443,	0.066580
+]
+
+# Create a form for user input
+st.write("## Predict Bird Type")
+
+with st.form(key='input_form'):
+    st.write("### Input Features")
+    feature_inputs = [st.number_input(feature_names[i], value=default_values[i]) for i in range(len(feature_names))]
+    submit_button = st.form_submit_button(label='Predict')  # Moved submit button here
+
+# Make prediction based on user input
+if submit_button:
+    user_data = np.array([feature_inputs])
+    user_data = loaded_scaler.transform(user_data)
     prediction = loaded_model.predict(user_data)
     bird_types = ['astfly', 'bulori', 'warvir', 'woothr']
     st.write("Predicted Bird Type:", bird_types[prediction[0]])
+
+# Add button to generate random values for input features
+if st.button("Generate Random Values"):
+    random_values = np.random.rand(len(feature_names))
+    for i in range(len(feature_names)):
+        feature_inputs[i].number_input(label=feature_names[i], value=random_values[i])
